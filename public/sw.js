8dcaf0631a80f8eb8d2eb2116622755b87839b8a
Samy Dam <Astralabus@gmail.com>
2025-06-25 12:46:18 +0000

    https://github.com/SamyDamerdji/Divinator/tree/main/icons   les fichiers

--- public/sw.js
+++ public/sw.js
@@ -1,45 +1,5 @@
-const CACHE_NAME = 'le-cartomancien-cache-v1';
-const urlsToCache = [
-  '/',
-  '/apprentissage',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+// This is a basic service worker file.
+// It's here primarily to make the app installable.
+self.addEventListener('fetch', function(event) {
+  // We are not caching anything in this simple service worker.
 });
