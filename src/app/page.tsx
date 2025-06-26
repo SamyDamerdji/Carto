0973950ab879b6d220eeee3e369e7b20b92e020d
Samy Dam <Astralabus@gmail.com>
2025-06-26 16:55:02 +0000

    La barre de navigation principale ne doit pas être affichée sur la page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Footer } from "@/components/layout/footer";
-import { Header } from "@/components/layout/header";
 import { Hero } from "@/components/sections/hero";
 
 export default function HomePage() {
   return (
     <div className="flex min-h-dvh flex-col">
-      <Header />
       <main className="flex-grow">
         <Hero />
       </main>
