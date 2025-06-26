53be7c813ca4105273512270aa45eef600912b7f
Samy Dam <Astralabus@gmail.com>
2025-06-26 12:41:15 +0000

    Ce n'est pas satisfaisant sur la photo que je t'ai transmis tu peux cons

--- src/components/cards/card-grid.tsx
+++ src/components/cards/card-grid.tsx
@@ -18,13 +18,13 @@ export function CardGrid({ cards }: CardGridProps) {
           <div className="absolute -right-2 -top-2 h-16 w-16 bg-[radial-gradient(closest-side,hsl(var(--primary)/0.1),transparent)]"></div>
           
           <div className="relative w-full aspect-[2.5/3.5] bg-card rounded-xl shadow-lg">
-            <div className="relative w-full h-full rounded-lg overflow-hidden p-1">
+            <div className="relative w-full h-full rounded-lg overflow-hidden">
               <div className="absolute inset-0 bg-gradient-to-tl from-black/60 via-transparent to-transparent z-10"></div>
               <Image
                 src={card.image_url}
                 alt={`Image de la carte ${card.nom_carte}`}
                 fill
-                className="object-contain"
+                className="object-contain p-2"
                 sizes="(max-width: 640px) 100vw, 50vw"
               />
             </div>
