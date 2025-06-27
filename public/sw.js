80ae3c0e51943cc27049c6b769d784ae39291d66
Samy Dam <Astralabus@gmail.com>
2025-06-27 22:07:38 +0000

    je suis sur Android

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,15 @@
+// This is a basic service worker file.
+// It's required for the app to be installable (PWA).
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installing...');
+  console.log('Service Worker installing.');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activating...');
+  console.log('Service Worker activating.');
 });
 
 self.addEventListener('fetch', (event) => {
-  // A basic fetch handler to make the app installable.
-  // This does not provide offline functionality.
+  // Basic fetch event handler
+  event.respondWith(fetch(event.request));
 });
